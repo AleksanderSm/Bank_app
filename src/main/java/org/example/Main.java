5a75ea11b8d593df938c4d9595d548e92abb4550
AleksanderSm <aleksander.smardzewski@gmail.com>
2022-12-05 18:25:59 +0100

    Java - dodanie nowej klasy User

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,19 @@
-package org.example;public class Main {
+package org.example;
+
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Witaj w naszym banku");
+        System.out.println("Wpisz nazwe uzytkownika");
+        Scanner scanner=new Scanner(System.in);
+        String name=scanner.nextLine();
+        System.out.println("Wprowadz nazwisko");
+        String surname=scanner.nextLine();
+        System.out.println("Wprowadz pesel");
+        int pesel=scanner.nextInt();
+
+        System.out.println("Menu\n 1.Informacje o koncie\n 2.Wplata pieniedzy \n 3.Wyplata pieniedzy \n4.Wyjscie  ");
     }
 }
--- src/main/java/org/example/User.java
+++ src/main/java/org/example/User.java
@@ -1,2 +1,18 @@
-package org.example;public class User {
+package org.example;
+
+public class User {
+    private String name;
+    private String surname;
+    private String nickname;
+    private int pesel;
+    private int accountNumber;
+    private double balance=100;
+
+    public User(String name, String surname, String nickname, int pesel, int accountNumber) {
+        this.name = name;
+        this.surname = surname;
+        this.pesel = pesel;
+
+
+    }
 }
